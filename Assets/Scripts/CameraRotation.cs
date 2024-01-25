2c4b88492363474c65042dc48afc291f3102dc0f
GarinIvan <ivangarin08@gmail.com>
2024-01-25 21:45:11 +0300

    2nd Commit

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public float RotationSpeed;
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3 (0, transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X"), 0);
+    }
+}
