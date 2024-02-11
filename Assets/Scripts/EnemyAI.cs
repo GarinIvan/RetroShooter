a5db40bb58f9aca1dd0cf9db0c2a1ed22beae0cc
GarinIvan <ivangarin08@gmail.com>
2024-02-11 16:43:33 +0300

    7th commit

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -5,15 +5,57 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    public Transform targetPoint;
+    public List<Transform> patrulPoint;
     private NavMeshAgent _navMeshAgent;
+    public PlayerController player;
+    private bool _isPlayerNoticed;
+    public float viewAngle;
     void Start()
     {
-        _navMeshAgent = GetComponent<NavMeshAgent>();
+        InitComponentLinks();
+        PatrolUpdate();
     }
     void Update()
     {
-        _navMeshAgent.destination = targetPoint.position;
+        NoticePlayerUpdate();
+        ChaseUpdate();
+        PatrolUpdate();
     }
-
+    private void NoticePlayerUpdate()
+    {
+        var direction = player.transform.position - transform.position;
+        _isPlayerNoticed = false;
+        if (Vector3.Angle(transform.forward, direction) < viewAngle)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + Vector3.up, direction, out hit))
+            {
+                if (hit.collider.gameObject == player.gameObject)
+                {
+                    _isPlayerNoticed = true;
+                }
+            }
+        }
+    }
+    private void ChaseUpdate()
+    {
+        if (_isPlayerNoticed)
+        {
+            _navMeshAgent.destination = player.transform.position;
+        }
+    }
+    private void PatrolUpdate()
+    {
+        if(!_isPlayerNoticed)
+        {
+            if (_navMeshAgent.remainingDistance == 0)
+            {
+                _navMeshAgent.destination = patrulPoint[Random.Range(0, patrulPoint.Count)].position;
+            }
+        }
+    }
+    private void InitComponentLinks()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
 }
